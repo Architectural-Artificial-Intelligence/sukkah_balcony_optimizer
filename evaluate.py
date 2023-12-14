ea707773603efd3272ee5295fa5d8ba03dbf65c2
Jonathan Dortheimer <jonathan@dortheimer.com>
2023-12-14 11:29:45 +0200

    Fixing code

--- evaluate.py
+++ evaluate.py
@@ -1,1 +1,64 @@
-from functions import main
+import sys 
+import json
+import functools 
+from shapely import geometry,Polygon, get_parts
+from functions import fileWrite, polygonRSformatter
+
+filename = sys.argv[1]
+
+input = open('generated/'+filename+'.json', 'r')
+
+levels = json.load(input)
+
+
+data = []
+
+
+for level in levels:
+    poly = []
+    for pt in level:
+        poly.append(geometry.shape(pt))
+    data.append(Polygon(poly))
+
+result =[]
+# convert to polygons
+
+
+# array of the floors, from the top
+floorStack = [data[len(data)-1]]
+
+#  run on all floors, beside the roof.
+for polygonsIndex in range(len(data)-1):
+    current = len(data) - polygonsIndex-1
+
+    balcony = data[polygonsIndex]
+
+        # this calculates the polygon difference
+    for currentBlaconyIndex in range(len(floorStack)):
+        try:
+            balcony = balcony.difference(floorStack[currentBlaconyIndex])
+
+            if balcony.is_empty:
+                break;
+        except:
+            exce = 1
+
+    floorStack.append(data[current])
+
+
+    # Save balconies sizes and number
+    if balcony.area>2:
+        balconiesItems = get_parts(balcony).tolist()
+        # balconies = list(map(lambda x: x.area, balconiesItems))
+        balconiesScore = 0
+        for bal in balconiesItems:
+            if (bal.area >=2):
+                balconiesScore+1
+        result.append({
+                'balconies':  balconiesScore,
+                # 'number': len(balconiesItems),
+                # 'area': data[current].area,
+                # 'scope':data[current].length
+        })
+
+print(result)
--- functions.py
+++ functions.py
@@ -1,18 +1,65 @@
 import pygad
 import numpy as np
-import random
+import statistics
 import math
 import time
 import json
-from shapely import Polygon, Point, get_num_geometries, get_parts
+from shapely import Polygon, MultiPolygon, Point, get_num_geometries, get_parts, geometry, unary_union, buffer, to_geojson, get_type_id
 
-# =================================================
-# functions
-# =================================================
 
-def generateRandom(boundingPolygon, howManyPoints, levels, levelOffset):
+# ============================
+# Data encoders
+
+def dataToGeomerty(data, numLevels):
     points = []
-    (minx, miny, maxx, maxy) = boundingPolygon.bounds
+    numPoints = int(len(data)/2)
+    pointsPerLevel = int(numPoints/numLevels)
+    z = 0
+    for index in range(numPoints):
+        if not index % pointsPerLevel:
+            z = z+3
+        points.append(Point(data[index*2], data[index*2+1], z))
+
+    byLevels = np.array_split(np.array(points), numLevels)
+
+    building = []
+    for level in byLevels:
+        building.append(buffer(Polygon(level),0))
+    
+    return building;
+
+def dataToFlatGeomerty(data, numLevels):
+    points = []
+    numPoints = int(len(data)/2)
+    pointsPerLevel = int(numPoints/numLevels)
+    for index in range(numPoints):
+        points.append(Point(data[index*2], data[index*2+1]))
+
+    byLevels = np.array_split(np.array(points), numLevels)
+
+    building = []
+    for level in byLevels:
+        poly = buffer(Polygon(level),0)
+        if (get_type_id(poly) != 3):
+            return []
+        
+        building.append(poly.convex_hull)
+    
+    return building;
+
+def geometryToData(points):
+    data = []
+    for point in points:
+        data.append(point.x)
+        data.append(point.y)
+        # removing z value
+    return data
+
+# ============================
+# Geometric functions
+
+def generateInitialSolutions(howManyPoints, levels, levelOffset):
+    points = []
     for level in range(levels):
         z = levelOffset*level
         sides = 4
@@ -32,225 +79,159 @@ def generateRandom(boundingPolygon, howManyPoints, levels, levelOffset):
 
     return points
 
+def getArea(elm):
+    return elm.area
 
-def renderRhinoFile(levels):
-    f = open('generated/'+str(time.time())+'.py', 'w')
-    fileWrite(f,"import rhinoscriptsyntax as rs")
-    fileWrite(f, "rs.EnableRedraw(False)")
-    fileWrite(f, "curves=[]")
-    fileWrite(f, "strPath = rs.AddLine([0,0,0], [0,0,3])")
+# ============================
+# File functions
 
-    y = 0
-    for level in levels:
-        pl = polygonRSformatter(level);
-        fileWrite(f, "curve"+str(y)+" = rs.AddPolyline(["+pl+"])")
-        # fileWrite(f, "rs.RebuildCurve ( curve"+str(y)+", degree=3, point_count=10)")
-        fileWrite(f, "curves.append(curve"+str(y)+")")
-        fileWrite(f, "rs.ExtrudeCurve(curve"+str(y)+", strPath)")
-        y+=1
-    
-    fileWrite(f, "rs.EnableRedraw(True)")
-    f.close()
+
+class NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if  isinstance(obj, Polygon):
+            return to_geojson(obj)
+        if  isinstance(obj, MultiPolygon):
+            return to_geojson(obj)
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        if isinstance(obj, Point):
+            return geometry.mapping(obj)
+        return json.JSONEncoder.default(self, obj)
+
 
 def renderJsonFile(levels):
-    data =[]
+    data = []
     for level in levels:
-        pl = polygonRSformatter(level);
-        data.append(pl)
+        # pl = polygonRSformatter(level);
+        data.append(level)
 
     with open('generated/'+str(time.time())+'.json', 'w') as f:
-        json.dump(data, f)
+        json.dump(data, f, cls=NumpyEncoder)
         f.close()
 
-def polygonRSformatter(points):
 
-    l = len(points)-1
-
-    if points[0].x!= points[l].x or points[0].y!=points[l].y:
-        points= np.append(points, points[0])
-    ptsArr = []
-
-    for point in points:
-        ptsArr.append("(%s,%s,%s)"%(point.x,point.y,point.z))
-    return ', '.join(ptsArr)
-    
-def fileWrite(f,txt):
-    f.seek(0,2)
-    f.write(txt+"\n\r")
-
-def data2points(data, numLevels):
-    points=[]
-    numPoints = int(len(data)/2)
-    pointsPerLevel = int(numPoints/numLevels)
-    z=0
-    for index in range(numPoints):
-        if not index % pointsPerLevel:
-            z=z+3
-        points.append(Point(data[index*2], data[index*2+1],z))
-
-    
-    
-    return np.array_split(np.array(points), numLevels)
-
-def points2data(points):
-    data=[]
-    for point in points:
-        data.append(point.x)
-        data.append(point.y)
-        # removing z value
-    return data
-
-def flattenPolygon(level):
-    points = []
-
-    for point in level:
-        points.append(Point(point.x,point.y))
-    
-    return Polygon(points)
-
-# Fitness functions
-def getEvaluationMetrics(solution):
-    building = data2points(solution, numLevels)
-    data = []
-    polygons = []
-
-    # convert to polygons
-    for level in building:
-        polygons.append(flattenPolygon(level))
-
-    # array of the floors, from the top
-    floorStack = [polygons[len(polygons)-1]]
-
-    #  run on all floors, beside the roof.
-    for polygonsIndex in range(len(polygons)-1):
-        current = len(polygons) - polygonsIndex-1
-
-        balcony = polygons[polygonsIndex]
-
-         # this calculates the polygon difference
-        for currentBlaconyIndex in range(len(floorStack)):
-            try:
-                balcony = balcony.difference(floorStack[currentBlaconyIndex])
-
-                if balcony.is_empty:
-                    break;
-            except:
-                exce = 1
-
-        floorStack.append(polygons[current])
-    
-    
-        # Save balconies sizes and number
-        if balcony.area>2:
-            balconiesItems = get_parts(balcony).tolist()
-            data.append({
-                    'balconies': list(map(lambda x: x.area, balconiesItems)) ,
-                    'number': len(balconiesItems),
-                    'scopeArea': polygons[current].area / polygons[current].length
-            })
-
-    return data
-
-# def calcLevelFitness(datum, p=False):
-#     levelScore = []
-#     # 4 balconies
-#     if (datum['number'] ==4): levelScore.append(1)
-#     else: levelScore.append(1/ abs(4-datum['number']))
-
-#     # 2 sqrm
-#     datum['balconies'].sort(reverse= True)
-#     for balcon in range(len(datum['balconies'])):
-#         if balcon > 4: break # no more than 4
-#         # if (datum['balconies'][balcon] >= 2): levelScore.append(1)
-#         # else: 
-#         bal = datum['balconies'][balcon];
-#         score = 10*bal+2-pow(bal,2)
-#         if p: print(score)
-#         levelScore.append(score)
-    
-#     levelScore.append(datum['scopeArea'])
-#     print(levelScore)
-#     return sum(levelScore)
-
-def calcLevelFitness(datum, p=False):
-    levelScore = []
-
-    # 4 balconies
-    # if (datum['number'] ==4): levelScore.append(1)
-    # else: levelScore.append(1/ abs(4-datum['number']))
-
-    # 2 sqrm
-    datum['balconies'].sort(reverse= True)
-    for balcon in range(len(datum['balconies'])):
-        if balcon >= 4: break # no more than 4
-        # if (datum['balconies'][balcon] >= 2): levelScore.append(1)
-        # else: 
-        bal = datum['balconies'][balcon];
-        if (bal>2): score = 1
-        else: score = 0
-        # score = 10*bal+2-pow(bal,2)
-        # if p: 
-        # print(bal, score)
-        levelScore.append(score)
-    
-    score = sum(levelScore) * datum['scopeArea']
-    return score
-
-
-
-# =================================================
-# Initial settings
-# =================================================
-
-num_generations = 100
-num_parents_mating = 50
-
-# how many chromosomes (values) exist in each individual
-parent_selection_type = "sss"
-keep_parents = 1
-
-crossover_type = "single_point"
-
-mutation_type = "random"
-mutation_percent_genes = 50
-# random_mutation_min_val=-3
-# random_mutation_max_val=3
-numPoints = 36
-numLevels = 30
-initiaOptions = 100
-
-thePlot = Polygon([(0,0), (35,0), (35,25), (0,35), (0,0)])
-
+# ============================
+# Main
 def main():
+
+    # =================================================
+    # Initial settings
+    # =================================================
+
+    num_generations = 200
+    initiaOptions = 50
+    num_parents_mating = 25
+
+    # how many chromosomes (values) exist in each individual
+    parent_selection_type = "sss"
+    keep_parents = 1
+
+    crossover_type = "single_point"
+
+    # mutation_type = "random"
+    # mutation_percent_genes = 10
+    mutation_type="adaptive"
+    mutation_percent_genes = [25, 12]
     
-    def fitness_func(ga_instance, solution, solution_idx):
-        data = getEvaluationMetrics(solution)
-        score = []
+    random_mutation_min_val = -2
+    random_mutation_max_val = 2
+    numPoints = 36
+    numLevels = 30
+    
 
-        for datum in data:
-            score.append(calcLevelFitness(datum))
-        return sum(score)
+    # Generate optimum building
+    optimum = np.full((numLevels-1,4), 4, dtype=float)
 
-    def on_gen(ga_instance):
-        best = ga_instance.best_solution()
-        print("Generation : ", ga_instance.generations_completed)
-        print("Fitness of the best solution :", best[1])
-        
-        # data = getEvaluationMetrics(best[0])
-        # print("======================")
-        # for level in range(len(data)):
-        #     score = calcLevelFitness(data[level])
-        #     print("Level",level, data[level])
-        #     print("Level score", score)
-        # print("======================")
-
-
-    # Holds the population
+    #  Generate solutions
     data = []
+
     # generate several random creatures
     for i in range(initiaOptions):
-        points = generateRandom(thePlot, numPoints, numLevels, 3)
-        data.append(points2data(points))
+        points = generateInitialSolutions(numPoints, numLevels, 3)
+        data.append(geometryToData(points))
+
+
+    
+    def fitness_func(ga_instance, solution, solution_idx):
+
+        # pts = generateInitialSolutions(numPoints,1,0)
+        # block = dataToFlatGeomerty(pts,1)
+        building = dataToFlatGeomerty(solution, numLevels)
+        if (not len(building)):
+            return 0
+        score = []
+        floorArea = []
+        scope = []
+        #  run on all floors, beside the roof.
+        for floorndex in range(len(building)-1):
+
+            score.append([0,0,0,0])
+
+            #  current floor
+            currentFloor = building[floorndex]
+            # Get roof
+            try:
+                roof = unary_union(building[floorndex::])
+                # roof = unary_union(block, roof)
+            except:
+                print("unary_union failed with") 
+                print(building[floorndex::])
+                continue;
+            try:
+                rest = currentFloor.difference(roof)
+                # rest = unary_union(block, rest)
+            except:
+                print("difference failed with") 
+                print(currentFloor, roof)
+                continue;
+            
+            if not currentFloor.length:
+                continue;
+            floorArea.append(currentFloor.area - rest.area) 
+            scope.append(currentFloor.area / currentFloor.length)
+
+            allBalconies = get_parts(rest).tolist()
+            # print(allBalconies)
+            
+            balconies = []
+            for bal in allBalconies:
+                balconies.append(bal.area)
+
+            balconies = sorted(balconies, reverse= True)
+            balconies = balconies[0:4:]
+
+            # print(len(building[floorndex::]),len(building), roof.area, rest.area, len(allBalconies) )
+            # print(balconies)
+            
+            if (balconies):
+                # print(balconies);
+                for index in range(len(balconies)):
+                    score[floorndex][index]=balconies[index]
+        
+       
+        # npScore = 1.0000000/ np.abs(np.array(score, dtype='f')- optimum)
+
+        npScore = np.sum(np.array(score, dtype='f')) * 10000000000000
+        # 1/abs(floorArea-numLevels*36*36) * 10
+        areaScore = 1 / abs( statistics.mean(floorArea) - numLevels*36*36) * 10000
+        scopeScore = statistics.mean(scope) / 10
+        # print("====")
+
+        # print(np.array(score))
+        # print(np.sum(npScore), 1/abs(floorArea-numLevels*36*36), floorArea)
+        # print("Score",  npScore, "areaScore", areaScore, "scopeScore", scopeScore)
+        # return 1000000* np.sum(npScore) + 1/abs(floorArea-numLevels*36*36) * 10
+        # return np.sum(npScore)
+        return npScore + areaScore + scopeScore 
+
+
+
+    def on_gen(ga_instance):
+        solution, solution_fitness, solution_idx = ga_instance.best_solution()
+        # print(f"Parameters of the best solution : {solution}")
+        print(f"Fitness value of the best solution = {solution_fitness}")
+        # print(f"Index of the best solution : {solution_idx}")
+
 
     ga_instance = pygad.GA(
         initial_population=data,
@@ -258,14 +239,11 @@ def main():
         num_parents_mating=num_parents_mating,
         fitness_func=fitness_func,
         on_generation=on_gen,
-        # sol_per_pop=sol_per_pop,
-        # num_genes=num_genes,
         parallel_processing=8,
-        # mutation_by_replacement=True,
-        # random_mutation_min_val=random_mutation_min_val,
-        # random_mutation_max_val=random_mutation_max_val,
-        stop_criteria="saturate_10",
-        gene_type=[float, 2],
+        random_mutation_min_val=random_mutation_min_val,
+        random_mutation_max_val=random_mutation_max_val,
+        # stop_criteria="saturate_10",
+        gene_type=int,
         parent_selection_type=parent_selection_type,
         keep_parents=keep_parents,
         crossover_type=crossover_type,
@@ -274,13 +252,14 @@ def main():
 
     ga_instance.run()
 
-    # ga_instance.plot_fitness(fontsize=10)
-
     solution, solution_fitness, solution_idx = ga_instance.best_solution()
-    print("Parameters of the best solution : {solution}".format(solution=solution))
+    print("Parameters of the best solution : {solution}".format(
+        solution=solution))
     print("Fitness value of the best solution = {solution_fitness}".format(
         solution_fitness=solution_fitness))
     print("Index of the best solution : {solution_idx}".format(
         solution_idx=solution_idx))
-    
-    renderJsonFile(data2points(solution, numLevels))
+
+    renderJsonFile(dataToGeomerty(solution, numLevels))
+
+
